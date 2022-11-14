6ca75d324d313f9aa841ebe50f3d18bdc1bd03c1
ekater1na <ekaterina_l@tut.by>
2022-11-14 21:16:15 +0300

    build: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className='text-bold underline text-green-900'>
-     Hello
-    </div>
-  );
+  return <div className="text-bold underline text-green-900">Hello</div>;
 }
 
 export default App;
