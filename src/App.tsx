3e4fe55b8a3482b1f8a87a65fe9e805ba4a1b850
ekater1na <ekaterina_l@tut.by>
2022-11-14 20:25:00 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { FavouritesPage } from './pages/FavouritesPage';
+import { Navigation } from './components/Navigation';
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/favourites' element={<FavouritesPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
