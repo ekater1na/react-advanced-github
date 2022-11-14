e805e3ff9c73aa146813cad87ea93c69bb3bff3a
ekater1na <ekaterina_l@tut.by>
2022-11-14 22:53:35 +0300

    feat: add debounce

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchUsersQuery } from '../store/github/github.api';
+import { useDebounce } from "../hooks/debounce";
 
 export const HomePage = () => {
-  const { isLoading, isError, data } = useSearchUsersQuery('kate');
-  return <div>Home</div>;
+  const [search, setSearch] = useState('');
+  const debounced = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3
+  });
+
+  useEffect(() => {
+    console.log(search);
+  }, [debounced]);
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && <p className="text-center text-red-600">Something went wrong</p>}
+
+      <div className="relative w-[560px]">
+        <input
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for Github username"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, magni.
+        </div>
+      </div>
+    </div>
+  );
 };
