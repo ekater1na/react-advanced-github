345661a70c80b24a4c16596cd4791f07db9be3ff
ekater1na <ekaterina_l@tut.by>
2022-11-15 00:15:27 +0300

    feat: add repo card

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLazyGetUserReposQuery, useSearchUsersQuery } from '../store/github/github.api';
 import { useDebounce } from '../hooks/debounce';
+import { RepoCard } from '../components/RepoCard';
 
 export const HomePage = () => {
   const [search, setSearch] = useState('');
@@ -20,6 +21,7 @@ export const HomePage = () => {
 
   const clickHandler = (username: string) => {
     fetchRepos(username);
+    setDropdown(false);
   };
 
   return (
@@ -49,8 +51,10 @@ export const HomePage = () => {
           </ul>
         )}
         <div className="container">
-          { areReposLoading && <p className="text-center">Repos are loading...</p> }
-          { repos?.map((repo) => <p>{repo.url}</p>) }
+          {areReposLoading && <p className="text-center">Repos are loading...</p>}
+          {repos?.map((repo) => (
+            <RepoCard repo={repo} key={repo.id} />
+          ))}
         </div>
       </div>
     </div>
