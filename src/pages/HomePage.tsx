f7a543b711031d1f6f67930bd9e2e835f84aa67e
ekater1na <ekaterina_l@tut.by>
2022-11-14 20:58:10 +0300

    build: update basic project structure

--- src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const HomePage = () => {
-  return (
-    <h1 className='text-3xl font-bold underline text-red-600 text-sm'>
-      Hello world!
-    </h1>
-  );
-};
